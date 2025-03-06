c2036d3e5617efb08fc81addff6c92e21a70bf91
Phan Thanh Bách <phanbach02@gmail.com>
2025-03-06 16:10:01 +0700

    [Feat] Setup bloc observer and device orientation

--- social_network_app/lib/main.dart
+++ social_network_app/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_network_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MainApp());
+  Bloc.observer = SimpleBlocObserver();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MainApp(FirebaseUserRepository()));
 }
