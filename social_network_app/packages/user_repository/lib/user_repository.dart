336dd34e43007cfe8bdf1e931f6a5f6b43f41b42
Phan Thanh Bách <phanbach02@gmail.com>
2025-03-08 18:47:04 +0700

    [Refactor] Change export file name

--- social_network_app/packages/user_repository/lib/user_repository.dart
+++ social_network_app/packages/user_repository/lib/user_repository.dart
@@ -3,4 +3,4 @@ library user_repository;
 export 'src/models/models.dart';
 export 'src/entities/entities.dart';
 export 'src/user_repo.dart';
-export 'src/firebase_user_repository.dart';
+export 'src/services_user_repository.dart';
