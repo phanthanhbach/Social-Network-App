14c4aa11b3041be162b124e08f912edc4cb93be6
Phan Thanh Bách <phanbach02@gmail.com>
2025-03-15 17:18:49 +0700

    [Feat] Define class PostEntity

--- social_network_app/packages/post_repository/lib/src/entities/post_entity.dart
+++ social_network_app/packages/post_repository/lib/src/entities/post_entity.dart
@@ -1,1 +1,52 @@
-class PostEntity {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:user_repository/user_repository.dart';
+
+class PostEntity {
+  String postId;
+  String post;
+  String? imageUrl;
+  DateTime createdAt;
+  MyUser myUser;
+
+  PostEntity({
+    required this.postId,
+    required this.post,
+    this.imageUrl,
+    required this.createdAt,
+    required this.myUser,
+  });
+
+  Map<String, Object?> toDocument() {
+    return {
+      'postId': postId,
+      'post': post,
+      'imageUrl': imageUrl,
+      'createdAt': createdAt,
+      'myUser': myUser.toEntity().toDocument(),
+    };
+  }
+
+  static PostEntity fromDocument(Map<String, dynamic> doc) {
+    return PostEntity(
+      postId: doc['postId'] as String,
+      post: doc['post'] as String,
+      imageUrl: doc['imageUrl'] as String?,
+      createdAt: (doc['createdAt'] as Timestamp).toDate(),
+      myUser: MyUser.fromEntity(MyUserEntity.fromDocument(doc['myUser'])),
+    );
+  }
+
+  @override
+  List<Object?> get props => [postId, post, imageUrl, createdAt, myUser];
+
+  @override
+  String toString() {
+    return '''PostEntity { 
+      postId: $postId, 
+      post: $post,
+      imageUrl: $imageUrl,
+      createdAt: $createdAt,
+      myUser: $myUser 
+    }''';
+  }
+}
