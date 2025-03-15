209432ae7a02246da402fc5005a089a903fe9d2f
Phan Thanh Bách <phanbach02@gmail.com>
2025-03-15 17:25:48 +0700

    [Feat] Add navigate and get posts to home scrreen

--- social_network_app/lib/screens/home/home_screen.dart
+++ social_network_app/lib/screens/home/home_screen.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:post_repository/post_repository.dart';
 import 'package:social_network_app/blocs/sign_in_bloc/sign_in_bloc.dart';
 
+import '../../blocs/create_post_bloc/create_post_bloc.dart';
+import '../../blocs/get_posts_bloc/get_posts_bloc.dart';
 import '../../blocs/my_user_bloc/my_user_bloc.dart';
 import '../../blocs/update_user_info_bloc/update_user_info_bloc.dart';
+import 'post_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,12 +33,41 @@ class _HomeScreenState extends State<HomeScreen> {
       },
       child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.surface,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
-            CupertinoIcons.add,
-            color: Theme.of(context).colorScheme.onSecondary,
-          ),
+        floatingActionButton: BlocBuilder<MyUserBloc, MyUserState>(
+          // Todo Gom BlocBuilder lại
+          builder: (context, state) {
+            if (state.status == MyUserStatus.success) {
+              return FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => BlocProvider<CreatePostBloc>(
+                        create: (context) => CreatePostBloc(
+                          postRepository: FirebasePostRepository(),
+                        ),
+                        child: PostScreen(
+                          state.user!,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                child: Icon(
+                  CupertinoIcons.add,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                ),
+              );
+            } else {
+              return FloatingActionButton(
+                onPressed: () {},
+                child: Icon(
+                  CupertinoIcons.add,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                ),
+              );
+            }
+          },
         ),
         appBar: AppBar(
           centerTitle: false,
@@ -130,70 +164,104 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: ListView.builder(
-            itemCount: 2,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  //color: Theme.of(context).colorScheme.secondary,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Theme.of(context).colorScheme.onSurface,
+        body: BlocBuilder<GetPostsBloc, GetPostsState>(
+          builder: (context, state) {
+            if (state is GetPostsSuccess) {
+              return ListView.builder(
+                  itemCount: state.posts.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        //height: 300,
+                        //color: Theme.of(context).colorScheme.secondary,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                          image: NetworkImage(state.posts[index].myUser.profilePicture!),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        state.posts[index].myUser.name,
+                                        style: TextStyle(
+                                          color: Theme.of(context).colorScheme.onPrimary,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        DateFormat('yyyy-MM-dd').format(state.posts[index].createdAt),
+                                        style: TextStyle(
+                                          color: Theme.of(context).colorScheme.onPrimary,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Container(
+                                child: Text(
+                                  state.posts[index].post,
+                                  textAlign: TextAlign.left,
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'User Name',
-                                  style: TextStyle(
-                                    color: Theme.of(context).colorScheme.onPrimary,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  'Posted 2 hours ago',
-                                  style: TextStyle(
-                                    color: Theme.of(context).colorScheme.onPrimary,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        Container(
-                          child: Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu tincidunt nisl. Nam posuere odio id lorem condimentum posuere. Nam et nisl ante. Cras molestie interdum magna nec sodales. Phasellus accumsan lectus non sapien laoreet porta. Cras ac magna auctor, tempor lacus et, fringilla erat.",
+                              state.posts[index].imageUrl != null
+                                  ? Padding(
+                                      padding: const EdgeInsets.only(top: 5),
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        height: 400,
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: NetworkImage(state.posts[index].imageUrl!),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  : Container(),
+                            ],
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                        ),
+                      ),
+                    );
+                  });
+            } else if (state is GetPostsLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            }),
+            } else {
+              return const Center(
+                child: Text('Error loading posts'),
+              );
+            }
+          },
+        ),
       ),
     );
   }
