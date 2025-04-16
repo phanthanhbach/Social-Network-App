ae4f32241be8cee14b093a1d1e9af59babee53a7
Phan Thanh Bách <phanbach02@gmail.com>
2025-04-16 15:34:40 +0700

    Update app_view.dart

--- social_network_app/lib/app_view.dart
+++ social_network_app/lib/app_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:post_repository/post_repository.dart';
-import 'package:social_network_app/blocs/get_posts_bloc/get_posts_bloc.dart';
+import 'package:social_network_app/blocs/my_user_bloc/my_user_bloc.dart';
+import 'package:social_network_app/screens/main_screen.dart';
 import 'blocs/authentication_bloc/authentication_bloc.dart';
-import 'blocs/my_user_bloc/my_user_bloc.dart';
-import 'blocs/sign_in_bloc/sign_in_bloc.dart';
-import 'blocs/update_user_info_bloc/update_user_info_bloc.dart';
-import 'screens/home/home_screen.dart';
+
 import 'screens/authentication/welcome_screen.dart';
 
 class MyAppView extends StatelessWidget {
@@ -18,47 +15,29 @@ class MyAppView extends StatelessWidget {
       title: 'Social Network App',
       theme: ThemeData(
         colorScheme: const ColorScheme.light(
-          primary: Color(0xFF2196F3),
-          onPrimary: Colors.black,
-          secondary: Colors.blueAccent,
-          onSecondary: Colors.white,
+          primary: Color(0xFFA594F9),
+          onPrimary: Colors.white,
+          secondary: Color(0xFFCDC1FF),
+          onSecondary: Colors.black,
           tertiary: Colors.green,
-          error: Colors.red,
-          surface: Colors.white,
+          error: Colors.redAccent,
+          surface: Color(0xFFF5EFFF),
+          surfaceContainer: Color(0xFFFBFBFB),
           onSurface: Colors.black,
           outline: Color(0xFF424242),
         ),
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.white,
           elevation: 3,
         ),
       ),
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return MultiBlocProvider(
-              providers: [
-                BlocProvider(
-                  create: (context) => SignInBloc(
-                    userRepository: context.read<AuthenticationBloc>().userRepository,
-                  ),
-                ),
-                BlocProvider(
-                  create: (context) => UpdateUserInfoBloc(
-                    userRepository: context.read<AuthenticationBloc>().userRepository,
-                  ),
-                ),
-                BlocProvider(
-                  create: (context) => MyUserBloc(
-                    myUserRepository: context.read<AuthenticationBloc>().userRepository,
-                  )..add(GetMyUser(myUserId: context.read<AuthenticationBloc>().state.user!.uid)),
-                ),
-                BlocProvider(
-                  create: (context) => GetPostsBloc(postRepository: FirebasePostRepository())..add(GetPosts()),
-                ),
-              ],
-              child: const HomeScreen(),
-            );
+            context.read<MyUserBloc>().add(
+                  GetMyUser(myUserId: state.user!.uid),
+                );
+            return const MainScreen();
           } else {
             return const WelcomeScreen();
           }
