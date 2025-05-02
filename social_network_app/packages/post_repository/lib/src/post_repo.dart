779a039d05273f4a4b5b807c713f51a1d6426a5a
Phan Thanh Bách <phanbach02@gmail.com>
2025-05-02 15:32:45 +0700

    [Feat] Add edit post func

--- social_network_app/packages/post_repository/lib/src/post_repo.dart
+++ social_network_app/packages/post_repository/lib/src/post_repo.dart
@@ -3,5 +3,7 @@ import 'models/models.dart';
 abstract class PostRepository {
   Future<Post> createPost(Post post, String? imagePath);
 
+  Future<Post> editPost(Post post, String imageUrl);
+
   Future<List<Post>> getPost();
 }
