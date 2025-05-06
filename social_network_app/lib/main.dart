b9789c03e6f81e0e926a71fdd1585ddaf162f1aa
Phan Thanh Bách <phanbach02@gmail.com>
2025-05-06 16:59:35 +0700

    [Feat] add load .env

--- social_network_app/lib/main.dart
+++ social_network_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:social_network_app/simple_bloc_observer.dart';
 import 'package:user_repository/user_repository.dart';
 import 'app.dart';
@@ -11,5 +12,6 @@ void main() async {
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await dotenv.load();
   runApp(MainApp(ServicesUserRepository()));
 }
